f38bb0d927bf2481a03ba8bf5f6f9b459f5b1e99
Samplications <Samplications@gmail.com>
2026-01-02 00:16:34 +0100

    updated config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()], // Only include react() if no conditional plugin is needed
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/flo_coaching",
-}));
+  base: "/flo_coaching/",
+});
