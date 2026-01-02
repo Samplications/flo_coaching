1766255ed9b15618ef8ae27d705d5048adb50d5a
Samplications <Samplications@gmail.com>
2026-01-02 20:39:44 +0100

    Update router basename to work with gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/flo_coaching">
         <Routes>
           <Route path="/" element={<Index/>} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
